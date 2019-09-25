2aede7046a1238451c8735f91dcd2ade07e1f4e1
masato.uno <masato.u20@gmail.com>
2019-09-25 12:23:58 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..100).each do |number|
+  Task.create(content: 'task_' + number.to_s, status: 'status_' + number.to_s)
+end
